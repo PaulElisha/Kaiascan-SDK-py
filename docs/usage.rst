29f11433e4570b2e22cb2a353d9f84450ada6a38
MayowaObisesan <mayowaobi74@gmail.com>
2024-11-26 23:42:01 -0800

    Kaiascan SDK py

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use kaiascan-sdk-py in a project::
+
+    import kaiascan_sdk_py
---
+++ src/kaiascan/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for kaiascan-sdk-py."""
+
+from .kaiascan import KaiascanSDK
+
+__author__ = """Mayowa Obisesan"""
+__email__ = "mayowaobi74@gmail.com"
+__version__ = "0.1.0"
+__all__ = ["KaiascanSDK"]
